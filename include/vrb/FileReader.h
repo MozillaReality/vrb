c462a1407e5cc27c641796664e5550463e07cf45
Randall Barker <rbarker@mozilla.com>
2017-12-09 12:06:58 -0800

    Adding FileReader interface

---
+++ include/vrb/FileReader.h
@@ -0,0 +1,42 @@
+#ifndef VRB_FILE_READER_DOT_H
+#define VRB_FILE_READER_DOT_H
+
+#include <string>
+#include <memory>
+
+namespace vrb {
+
+class FileHandler;
+typedef std::shared_ptr<FileHandler> FileHandlerPtr;
+class FileReader;
+typedef std::shared_ptr<FileReader> FileReaderPtr;
+
+class FileHandler {
+public:
+  virtual void Bind(const std::string& aFileName, const int aFileHandle) = 0;
+  virtual void LoadFailed(const int aFileHandle, const std::string& aReason) = 0;
+  virtual void ProcessBuffer(const int aFileHandle, const char* aBuffer, const size_t aSize) = 0;
+  virtual void Finish(const int aFileHandle) = 0;
+protected:
+  FileHandler() {}
+  virtual ~FileHandler() {}
+private:
+  FileHandler(const FileHandler&) = delete;
+  FileHandler& operator=(const FileHandler&) = delete;
+};
+
+class FileReader {
+public:
+  virtual void ReadRawFile(const std::string& aFileName, FileHandlerPtr aHandler) = 0;
+  virtual void ReadImageFile(const std::string& aFileName, FileHandlerPtr aHandler) = 0;
+protected:
+  FileReader() {}
+  virtual ~FileReader() {}
+private:
+  FileReader(const FileReader&) = delete;
+  FileReader& operator=(const FileReader&) = delete;
+};
+
+}
+
+#endif // VRB_FILE_READER_DOT_H
