a455642444f6f2d851842b5e82788a485503a43e
Manuel Martin <imanol.martin@gmail.com>
2018-08-07 22:51:32 +0200

    Remove all log messages but error and warning when in release to match how we are doing it in Java (#43)

--- include/vrb/Logger.h
+++ include/vrb/Logger.h
@@ -8,10 +8,14 @@
 
 #if defined(ANDROID)
 #include <android/log.h>
-#define VRB_LOG(format, ...) __android_log_print(ANDROID_LOG_INFO, "VRB", format, ##__VA_ARGS__);
 #define VRB_WARN(format, ...) __android_log_print(ANDROID_LOG_WARN, "VRB", format, ##__VA_ARGS__);
 #define VRB_ERROR(format, ...) __android_log_print(ANDROID_LOG_ERROR, "VRB", format, ##__VA_ARGS__);
-#define VRB_LINE VRB_LOG("%s:%s:%d", __FILE__, __FUNCTION__, __LINE__)
+#ifdef NDEBUG
+#define VRB_LOG(...);
+#else
+#define VRB_LOG(format, ...) __android_log_print(ANDROID_LOG_INFO, "VRB", format, ##__VA_ARGS__);
+#endif
+#define VRB_LINE VRB_LOG("%s:%s:%d", __FILE__, __FUNCTION__, __LINE__);
 #else
 #include <stdio.h>
 #define VRB_LOG(format, ...) fprintf(stderr, "VRB: " format "\n", ##__VA_ARGS__);
