33c0721cf635a9778d282dfb7ffd62b9f3ee2fe4
Randall E. Barker <rbarker@mozilla.com>
2019-04-02 13:18:19 -0700

    Set thread names

---
+++ include/vrb/ThreadUtils.h
@@ -0,0 +1,15 @@
+/* -*- Mode: C++; tab-width: 20; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef VRB_THREAD_UTILS_DOT_H
+#define VRB_THREAD_UTILS_DOT_H
+
+#include <string>
+
+namespace vrb {
+void SetThreadName(const std::string& aName);
+}
+
+#endif // VRB_THREAD_UTILS_DOT_H
