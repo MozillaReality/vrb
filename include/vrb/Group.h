e901a26d3fb2b8ef92b99bb794f90f1f2fdb426a
Randall E. Barker <simstorm@mac.com>
2019-08-23 08:30:49 -0700

    Add support for prerender and postrender lambdas (#70)

--- include/vrb/Group.h
+++ include/vrb/Group.h
@@ -29,6 +29,8 @@ public:
   int32_t GetNodeCount() const;
   void SortNodes(const std::function<bool(const vrb::NodePtr&, const vrb::NodePtr&)>& aFunction);
   void TakeChildren(GroupPtr& aGroup);
+  void SetPreRenderLambda(const RenderLambda& aLambda);
+  void SetPostRenderLambda(const RenderLambda& aLambda);
 
 protected:
   bool Traverse(const GroupPtr& aParent, const Node::TraverseFunction& aTraverseFunction) override;
